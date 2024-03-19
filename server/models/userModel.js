c2783c6ff39eddfd0a3cc6a90eed818e0554248c
lawrence-254 <lodhiambo26@gmail.com>
2024-03-19 21:40:12 +0300

    userschema

---
+++ server/models/userModel.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+
+// Define the schema for the user model
+const userSchema = mongoose.Schema(
+    {
+        name: { type: String, required: true },
+        email: { type: String, required: true },
+        password: { type: String, required: true },
+        picture: { type: String, required: true, default: "../static/default.jpeg" }
+    }
+);
+
+// Create a model based on the schema
+const User = mongoose.model('User', userSchema);
+
+// Export the model
+module.exports = User;
