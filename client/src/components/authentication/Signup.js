146385f22c2731a703ebc3927a515c99b8ca31fe
lawrence-254 <lodhiambo26@gmail.com>
2024-03-20 16:02:15 +0300

    form

--- client/src/components/authentication/Signup.js
+++ client/src/components/authentication/Signup.js
@@ -1,10 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Box, FormControl, FormLabel, Input, StackDivider, VStack } from '@chakra-ui/react';
+
 
 function Signup() {
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [profilePic, setProfilePic] = useState('');
+    const [confirmPassword, setConfirmPassword] = useState('');
     return (
-        <div>
-            signup
-        </div>
+        <VStack
+            divider={<StackDivider borderColor='gray.200' />}
+            spacing={4}
+            align='stretch'
+        >
+            <FormControl>
+                <FormLabel>Name</FormLabel>
+                <Input type='text' placeholder='Your name' onChange={(e) => {
+                    setName(e.target.value);
+                }} />
+            </FormControl>
+
+        </VStack>
     )
 }
 
--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -10,8 +10,8 @@ function HomePage() {
         // alignItems="center"
         // height="100vh"
         >
-            <Box backgroundColor='teal' fontFamily='Style Script' fontWeight='400' borderRadius='md' p={4} m={4} fontSize="xl">
-                <Text fontSize="xl">Twitter</Text>
+            <Box backgroundColor='teal' fontFamily='Style Script' fontWeight='400' borderRadius='md' width='50vw' p={4} m={4} fontSize="xl">
+                <Text fontSize="50px" textAlign="center" align="center">Twitter</Text>
             </Box>
             <Box borderRadius='md' m={4}>
                 <Text></Text>
@@ -20,7 +20,7 @@ function HomePage() {
                         <Tab>LOGIN</Tab>
                         <Tab>SIGNUP</Tab>
                     </TabList>
-                    <TabPanels>
+                    <TabPanels backgroundColor='teal' width='50vw'>
                         <TabPanel>
                             <Login />
                         </TabPanel>
