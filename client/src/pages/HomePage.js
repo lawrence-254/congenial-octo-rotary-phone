2e8301d0584049dbf41974d2e3d9b2c06ce9b91a
lawrence-254 <lodhiambo26@gmail.com>
2024-03-20 14:23:29 +0300

    components

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -1,10 +1,36 @@
 import React from 'react'
+import { Box, Container, Text, Tabs, TabList, TabPanels, Tab, TabPanel } from '@chakra-ui/react';
+import Login from '../components/authentication/Login';
+import Signup from '../components/authentication/Signup';
 
 function HomePage() {
     return (
-        <div>
-            Home
-        </div>
+        <Container centerContent
+        // justifyContent="center"
+        // alignItems="center"
+        // height="100vh"
+        >
+            <Box backgroundColor='teal' fontFamily='Style Script' fontWeight='400' borderRadius='md' p={4} m={4} fontSize="xl">
+                <Text fontSize="xl">Twitter</Text>
+            </Box>
+            <Box borderRadius='md' m={4}>
+                <Text></Text>
+                <Tabs variant='soft-rounded' colorScheme='green'>
+                    <TabList mb='1em'>
+                        <Tab>LOGIN</Tab>
+                        <Tab>SIGNUP</Tab>
+                    </TabList>
+                    <TabPanels>
+                        <TabPanel>
+                            <Login />
+                        </TabPanel>
+                        <TabPanel>
+                            <Signup />
+                        </TabPanel>
+                    </TabPanels>
+                </Tabs>
+            </Box>
+        </Container>
     )
 }
 
