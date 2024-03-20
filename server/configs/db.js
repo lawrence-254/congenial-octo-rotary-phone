e48edf33c8fa54427baf55ddf1c9909d60dbd7b2
lawrence-254 <lodhiambo26@gmail.com>
2024-03-20 20:22:37 +0300

    dbconfigs

--- server/configs/db.js
+++ server/configs/db.js
@@ -1,1 +1,16 @@
 const mongoose = require('mongoose');
+
+const dbConnection = async () => {
+    try {
+        const connection = await mongoose.connect(process.env.MONGO_DB_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+        console.log(`Connected to MongoDB' ${conn.connection.host}`);
+    } catch (error) {
+        console.error('Error connecting to MongoDB', error);
+        process.exit();
+    }
+}
+
+module.exports = dbConnection;
