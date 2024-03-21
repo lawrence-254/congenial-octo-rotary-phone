ad6099c8e49ed9c42311a812ec5a0f27739673ff
lawrence-254 <lodhiambo26@gmail.com>
2024-03-21 21:06:32 +0300

    update

--- client/src/context/ChatProvider.js
+++ client/src/context/ChatProvider.js
@@ -1,18 +1,28 @@
-import { createContext, useContext } from 'react';
-
-
+import { createContext, useContext, useEffect, useState } from 'react';
+import { useHistory } from 'react-router-dom';
 
 const ChatContext = createContext();
 
 const ChatProvider = ({ children }) => {
     const [user, setUser] = useState(null);
+    const history = useHistory();
+
+    useEffect(() => {
+        const userInfo = localStorage.getItem('user');
+        if (userInfo) {
+            setUser(JSON.parse(userInfo));
+        } else {
+            history.push('/');
+        }
+    }, []);
+
     return (
-        <ChatContext.Provider value={{}}>
+        <ChatContext.Provider value={{ user, setUser }}>
             {children}
         </ChatContext.Provider>
     );
 }
-export const chatState = () => {
+export const ChatState = () => {
     return useContext(ChatContext);
 }
 export default ChatProvider;
