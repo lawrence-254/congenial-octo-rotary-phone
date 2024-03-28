b718aa61f21f32281da7c96c5ed08abb67c209ef
lawrence-254 <lodhiambo26@gmail.com>
2024-03-28 23:30:03 +0300

    messageapi

--- server/controllers/messageController.js
+++ server/controllers/messageController.js
@@ -4,6 +4,26 @@ const Chat = require("../models/chatModel");
 const User = require("../models/userModel");
 
 
-const sendMessage = expressAsyncHandler(() => {
+const sendMessage = expressAsyncHandler(async (req, res) => {
+    const { chatId, content } = req.body;
 
-})
+    if (!chatId || !content) {
+        res.status(400);
+        throw new Error("Chat ID and message are required");
+        console.log("Chat ID and message are required");
+    }
+
+    var newMessage = {
+        chat: chatId,
+        sender: req.user._id,
+        content: content
+    };
+
+    try { }
+    catch (error) {
+        res.status(500);
+        throw new Error("Error sending message");
+    }
+});
+
+module.exports = { sendMessage };
